1113053b8d5ee9b47c38eb7ffcc40f2f453ebfde
John Huddleston <huddlej@gmail.com>
2012-08-06 09:56:27 -0700

    track experiment number for all actions.

--- tracker.py
+++ tracker.py
@@ -3,9 +3,10 @@ Tool for tracking behavioral actions and timestamping when those actions occur.
 """
 import csv
 from datetime import datetime
+import sys
 
 
-def main():
+def main(experiment):
     fh = open("experiments.tab", "a")
     writer = csv.writer(fh, delimiter="\t")
 
@@ -25,15 +26,19 @@ def main():
             print help
             cmd = raw_input("> ")
             now = datetime.now()
+
+            print cmd_map[cmd], now
+            writer.writerow((str(experiment), cmd_map[cmd], now))
+
+            if cmd == "e":
+                experiment += 1
         except EOFError:
             print "Exiting"
             break
-
-        print cmd_map[cmd], now
-        writer.writerow((cmd_map[cmd], now))
 
     fh.close()
 
 
 if __name__ == "__main__":
-    main()
+    experiment = int(sys.argv[1])
+    main(experiment)
